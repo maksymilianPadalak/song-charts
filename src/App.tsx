7a88657b8108b03be6e30bdb63c97765a964a04b
Maksymilian <maksymilian.padalak@gmail.com>
2022-10-05 22:30:54 +0200

    Add es-linter and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <div className="App">
-        <h1>Test</h1>
+    <div className='App'>
+      <h1>Test</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
