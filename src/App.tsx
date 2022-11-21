d6bb7ee42f9d98639e00eaad3e288adeb3001fd0
Maksymilian <maksymilian.padalak@gmail.com>
2022-11-22 00:01:33 +0100

    Add React Bootstrap, Bootstrap and Sass to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import './App.css';
-
 function App() {
   return (
     <div className='App'>
-      <h1>Test</h1>
+      <h1>Monika Juskowiak jest najpiękniejszą kobietą na świecie</h1>
+      <h2>UWIELBIAM JĄ</h2>
     </div>
   );
 }
