d031c4791961de760d35825f34412378c35a37b6
Maksymilian <maksymilian.padalak@gmail.com>
2022-11-26 12:15:36 +0100

    Create exemplary chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import RappersCarousel from './landingPage/RappersCarousel';
+import MainChart from './charts/components/MainChart';
 
 const App: React.FC = () => {
-  return <RappersCarousel />;
+  return (
+    <div>
+      <RappersCarousel />
+      <div style={{ height: '500px' }}>
+        <MainChart />
+      </div>
+    </div>
+  );
 };
 
 export default App;
