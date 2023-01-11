5490a63a1f8ac037673d8d7aa20068b358ecbecb
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-11 19:18:22 +0100

    Implement react router dom library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import LandingPage from './landingPage/components/LandingPage';
+import { Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
-  return <LandingPage className='min-vh-100' />;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LandingPage className='min-vh-100' />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
