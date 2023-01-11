7a7ef863f9012183e9c0fb5e22aaca3458d2d87b
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-11 20:57:29 +0100

    Create login routes based on token

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import LandingPage from './landingPage/components/LandingPage';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import AdminDashboard from './admin/components/AdminDashboard';
+import Login from './admin/components/Login';
 
 const App: React.FC = () => {
+  const token = '';
   return (
     <div>
       <Routes>
-        <Route path='/' element={<LandingPage className='min-vh-100' />} />
-        <Route path='/admin' element={<AdminDashboard />} />
+        <Route path={'/'} element={<LandingPage className='min-vh-100' />} />
+        <Route path={'/admin'}>
+          {token ? (
+            <>
+              <Route path={'dashboard'} element={<AdminDashboard />} />
+              <Route path='' element={<Navigate to={'dashboard'} />} />
+              <Route path='*' element={<Navigate to={'dashboard'} />} />
+            </>
+          ) : (
+            <>
+              <Route path={'login'} element={<Login />} />
+              <Route path='' element={<Navigate to={'login'} />} />
+              <Route path='*' element={<Navigate to={'login'} />} />
+            </>
+          )}
+        </Route>
       </Routes>
     </div>
   );
