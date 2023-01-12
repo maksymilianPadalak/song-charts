a4b8b79cce315982fcc3f5dfbfc15bbae4d0357f
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-12 22:00:38 +0100

    Create preliminary SongDetails UI

--- src/admin/components/SongDetails.tsx
+++ src/admin/components/SongDetails.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 
 const SongDetails: React.FC = () => {
-  return <h1>This are song details</h1>;
+  return (
+    <div className={'d-flex flex-column align-self-center text-center'}>
+      <h1>Petki</h1>
+      <div className={'d-flex justify-content-around'}>
+        <div className={'d-flex'}>
+          <h3>Artist:</h3>
+          <h3>Mata</h3>
+        </div>
+        <div className={'d-flex'}>
+          <h3>Album:</h3>
+          <h3>100 dni do matury</h3>
+        </div>
+      </div>
+      <div className={'m-5'}>
+        <Form.Control as='textarea'></Form.Control>
+      </div>
+      <button className={'btn btn-dark w-50 align-self-center'}>Submit changes</button>
+      <Link className={'btn btn-light w-50 align-self-center mt-3'} to={'admin/dashboard'}>
+        Cancel
+      </Link>
+    </div>
+  );
 };
 
 export default SongDetails;
