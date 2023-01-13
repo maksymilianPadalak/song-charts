c5bd4f1bfe42672ae3e0d7d0d2fe5425721c08be
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-13 22:16:27 +0100

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/admin': {
-        target: 'https://lyrics-373516.ew.r.appspot.com',
-        changeOrigin: true,
-      },
       '/api': {
         target: 'https://lyrics-373516.ew.r.appspot.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
