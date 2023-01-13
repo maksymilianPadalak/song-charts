a5e296ccf8e82038de6edabc143471514113312e
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-13 20:08:46 +0100

    Create error component

---
+++ src/shared/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error: React.FC = () => {
+  return (
+    <div className='d-flex justify-content-center text-center'>
+      <h3>Sorry :( We are unable to fetch data</h3>
+    </div>
+  );
+};
+
+export default Error;
