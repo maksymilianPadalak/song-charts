6ef471227a653f5a5dd6df813ba53d9d7ea1b6db
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-13 20:09:13 +0100

    Implement error component in songs list

--- src/admin/components/SongsList.tsx
+++ src/admin/components/SongsList.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import SongsListItem from './SongListItem';
 import Pagination, { bootstrap5PaginationPreset } from 'react-responsive-pagination';
+import { useQuery } from 'react-query';
+import { queryKeys } from '../../api/queryKeys';
+import { fetchPosts } from '../../api/admin/adminApiFunctions';
+import Loader from '../../shared/components/Loader';
+import Error from '../../shared/components/Error';
 
 interface Props {
   className?: string;
@@ -10,14 +15,19 @@ const x = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const SongsList: React.FC<Props> = ({ className }) => {
   const itemsPerPage = 5;
+  const totalPages = Math.ceil(x.length / itemsPerPage);
+
+  const { data, isLoading, isError } = useQuery(queryKeys.songs, () => fetchPosts());
   const [currentPage, setCurrentPage] = useState(1);
   const [currentItems, setCurrentItems] = useState(x.slice(0, itemsPerPage));
-  const totalPages = Math.ceil(x.length / itemsPerPage);
 
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
     setCurrentItems(x.slice(pageNumber * itemsPerPage - itemsPerPage, pageNumber * itemsPerPage));
   };
+
+  if (isLoading) return <Loader />;
+  if (isError) return <Error />;
 
   return (
     <div className={className}>
