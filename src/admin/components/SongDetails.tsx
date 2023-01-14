ea1aa32d7f54eee9b768f28d00b593af21904589
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-14 17:56:53 +0100

    Style song details and add full screen option to loader

--- src/admin/components/SongDetails.tsx
+++ src/admin/components/SongDetails.tsx
@@ -14,26 +14,32 @@ const SongDetails: React.FC = () => {
     cacheTime: 0,
   });
 
-  if (isLoading) return <Loader size={'lg'} />;
+  if (isLoading) return <Loader size={'lg'} fullScreen />;
 
   return (
     <div className={'d-flex flex-column align-self-center text-center'}>
-      <h1>{data?.songTitle}</h1>
-      <div className={'d-flex justify-content-around'}>
-        <div className={'d-flex'}>
-          <h3>Artist:</h3>
+      <div className={'my-3 my-md-4'}>
+        <label>Song</label>
+        <h1>{data?.songTitle}</h1>
+      </div>
+      <div className={'d-none d-md-flex justify-content-around'}>
+        <div>
+          <label>Artist</label>
           <h3>{data?.author}</h3>
         </div>
-        <div className={'d-flex'}>
-          <h3>Album:</h3>
+        <div>
+          <label>Album</label>
           <h3>{data?.albumTitle}</h3>
         </div>
       </div>
-      <div className={'m-5'}>
-        <Form.Control as='textarea' value={data?.cleanedLyrics} />
+      <div className={'m-2 m-md-5'}>
+        <Form.Control as='textarea' className={'text-area'} value={data?.cleanedLyrics} />
       </div>
-      <button className={'btn btn-dark w-50 align-self-center'}>Submit changes</button>
-      <Link className={'btn btn-light w-50 align-self-center mt-3'} to={'admin/dashboard'}>
+      <button className={'btn btn-dark col-11 col-sm-6 align-self-center'}>Submit changes</button>
+      <Link
+        className={'btn btn-light col-11 col-sm-6 align-self-center mt-3'}
+        to={'admin/dashboard'}
+      >
         Cancel
       </Link>
     </div>
--- src/shared/components/Loader.tsx
+++ src/shared/components/Loader.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 interface Props {
   size?: 'sm' | 'md' | 'lg';
+  fullScreen?: boolean;
 }
 
-const Loader: React.FC<Props> = ({ size = 'md' }) => {
+const Loader: React.FC<Props> = ({ size = 'md', fullScreen = false }) => {
   return (
-    <div className='loader-container'>
+    <div className={`loader-container${fullScreen ? '-full-screen' : ''}`}>
       <div className={`spinner-${size}`} />
     </div>
   );
