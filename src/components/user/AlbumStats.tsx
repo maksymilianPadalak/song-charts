78491a18eb867030019fa6a05f5436e81aa7a446
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-14 22:40:51 +0100

    Add album title to AlbumsStats

--- src/components/user/AlbumStats.tsx
+++ src/components/user/AlbumStats.tsx
@@ -1,13 +1,19 @@
 import MainChart from '../charts/MainChart';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 interface Props {
   className?: string;
 }
 
 const AlbumStats: React.FC<Props> = ({ className }) => {
+  const { title } = useParams() as { title: string };
+
   return (
-    <div className={`${className} d-flex flex-column justify-content-between`}>
+    <div className={`${className}`}>
+      <div className={'d-flex justify-content-center m-3'}>
+        <h1>{title}</h1>
+      </div>
       <div style={{ height: '500px' }}>
         <MainChart />
       </div>
