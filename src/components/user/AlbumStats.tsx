15ed41da2bb5fac9f095e4e4a76fda065132c72a
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-15 15:59:46 +0100

    Create go back to albums button

--- src/components/user/AlbumStats.tsx
+++ src/components/user/AlbumStats.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { reactQueryKeys } from '../../api/user/reactQueryKeys';
 import { fetchAlbumStats } from '../../api/user/apiFunctions';
@@ -17,6 +17,14 @@ const AlbumStats: React.FC<Props> = ({ className }) => {
     reactQueryKeys.albumStats,
     () => fetchAlbumStats({ albumTitle: title }),
     { cacheTime: 0 },
+  );
+
+  const goBackToAlbumsButton = (
+    <div className={'m-4 d-flex justify-content-center'}>
+      <Link to={'/'} className={'btn btn-warning m-4 col-12 col-md-4'}>
+        Go back to albums list
+      </Link>
+    </div>
   );
 
   if (isLoading) return <Loader fullScreen />;
@@ -43,8 +51,9 @@ const AlbumStats: React.FC<Props> = ({ className }) => {
 
   if (statsByArtist.length === 0)
     return (
-      <div className={'d-flex min-vh-100 align-items-center justify-content-center'}>
+      <div className={'d-flex flex-column min-vh-100 justify-content-center text-center'}>
         <h2>We are unable to display data about this track :(</h2>
+        {goBackToAlbumsButton}
       </div>
     );
 
@@ -63,11 +72,17 @@ const AlbumStats: React.FC<Props> = ({ className }) => {
           >
             <h3 className={'mt-2'}>Artist: {artistName}</h3>
             <MainChart mostPopularWords={stat[artistName]} />
-            <h5 className={'mb-2'}>Included songs</h5>
-            <div>{stats?.includedSongs.join(', ')}</div>
           </div>
         );
       })}
+      <div className={'d-flex flex-column align-items-center text-center'}>
+        <h5 className={'mb-2'}>Included songs</h5>
+        <div className={'mx-3'}>
+          {/*@ts-ignore*/}
+          {stats?.includedSongs.join(', ')}
+        </div>
+      </div>
+      {goBackToAlbumsButton}
     </div>
   );
 };
