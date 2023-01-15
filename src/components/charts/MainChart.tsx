c1f6f5ddd756842102f624ca20fbf0c7856d4c75
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-15 15:10:41 +0100

    Display data about songs on charts

--- src/components/charts/MainChart.tsx
+++ src/components/charts/MainChart.tsx
@@ -2,82 +2,75 @@ import React from 'react';
 import { ResponsiveBar } from '@nivo/bar';
 
 interface Props {
-  data?: any;
+  mostPopularWords: Record<string, number>;
 }
 
-const examplaryData = [
-  {
-    'hot dog': 80,
-    burger: 40,
-    sandwich: 83,
-    kebab: 166,
-    fries: 197,
-    donut: 94,
-  },
-];
-
-const MainChart: React.FC<Props> = ({ data = examplaryData }) => (
-  <ResponsiveBar
-    data={data}
-    keys={Object.keys(data[0])}
-    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-    innerPadding={10}
-    groupMode='grouped'
-    valueScale={{ type: 'linear' }}
-    indexScale={{ type: 'band', round: true }}
-    colors={{ scheme: 'nivo' }}
-    borderColor={{ theme: 'background' }}
-    axisBottom={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legendPosition: 'middle',
-      legendOffset: 32,
-    }}
-    axisLeft={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'food',
-      legendPosition: 'middle',
-      legendOffset: -40,
-    }}
-    labelSkipWidth={8}
-    labelTextColor={{
-      from: 'color',
-      modifiers: [['darker', 1.8]],
-    }}
-    legends={[
-      {
-        dataFrom: 'keys',
-        anchor: 'right',
-        direction: 'column',
-        justify: false,
-        translateX: 120,
-        translateY: 0,
-        itemsSpacing: 2,
-        itemWidth: 100,
-        itemHeight: 20,
-        itemDirection: 'left-to-right',
-        itemOpacity: 0.85,
-        symbolSize: 20,
-        effects: [
-          {
-            on: 'hover',
-            style: {
-              itemOpacity: 1,
+const MainChart: React.FC<Props> = ({ mostPopularWords }) => {
+  const examplaryData = [mostPopularWords];
+  return (
+    <ResponsiveBar
+      data={examplaryData}
+      keys={Object.keys(examplaryData[0])}
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      innerPadding={10}
+      tooltip={({ label }) => (
+        <div className={'bg-light p-2'}>
+          <h3>{label.split(' ').slice(0, 1)}</h3>
+        </div>
+      )}
+      groupMode='grouped'
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'nivo' }}
+      borderColor={{ theme: 'background' }}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legendPosition: 'middle',
+        legendOffset: 32,
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'words',
+        legendPosition: 'middle',
+        legendOffset: -40,
+      }}
+      labelSkipWidth={8}
+      labelTextColor={{
+        from: 'color',
+        modifiers: [['darker', 1.8]],
+      }}
+      legends={[
+        {
+          dataFrom: 'keys',
+          anchor: 'right',
+          direction: 'column',
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: 'left-to-right',
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemOpacity: 1,
+              },
             },
-          },
-        ],
-      },
-    ]}
-    motionConfig='wobbly'
-    role='application'
-    ariaLabel='Nivo bar chart demo'
-    barAriaLabel={function (e) {
-      return e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue;
-    }}
-  />
-);
+          ],
+        },
+      ]}
+      motionConfig='wobbly'
+      role='application'
+    />
+  );
+};
 
 export default MainChart;
--- src/components/user/AlbumStats.tsx
+++ src/components/user/AlbumStats.tsx
@@ -1,10 +1,10 @@
-import MainChart from '../charts/MainChart';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { reactQueryKeys } from '../../api/user/reactQueryKeys';
 import { fetchAlbumStats } from '../../api/user/apiFunctions';
 import Loader from '../shared/Loader';
+import MainChart from '../charts/MainChart';
 
 interface Props {
   className?: string;
@@ -13,23 +13,54 @@ interface Props {
 const AlbumStats: React.FC<Props> = ({ className }) => {
   const { title } = useParams() as { title: string };
 
-  const { data, isLoading } = useQuery(reactQueryKeys.albumStats, () =>
-    fetchAlbumStats({ albumTitle: title }),
+  const { data, isLoading } = useQuery(
+    reactQueryKeys.albumStats,
+    () => fetchAlbumStats({ albumTitle: title }),
+    { cacheTime: 0 },
   );
 
   if (isLoading) return <Loader fullScreen />;
 
-  const stats = data || {};
+  const stats = data;
   const artists: string[] = Object.keys(stats).filter((key) => key !== 'includedSongs');
+
+  const statsByArtist = artists.map((artist) => ({
+    [artist]: Object.keys(stats[artist]?.freq)
+      .map((key) => ({ [key]: stats[artist]?.freq[key] }))
+      .sort((a, b) => Object.values(b) - Object.values(a))
+      .slice(0, 10)
+      .reduce((acc, cur) => {
+        const key = Object.keys(cur)[0];
+        acc[key] = cur[key];
+        return acc;
+      }, {} as Record<string, number>),
+  }));
+
+  if (statsByArtist.length === 0)
+    return (
+      <div className={'d-flex min-vh-100 align-items-center justify-content-center'}>
+        <h2>We are unable to display data about this track :(</h2>
+      </div>
+    );
 
   return (
     <div className={`${className}`}>
-      <div className={'d-flex justify-content-center m-3'}>
+      <div className={'d-flex justify-content-center m-3 text-center'}>
         <h1>{title}</h1>
       </div>
-      <div style={{ height: '500px' }}>
-        <MainChart />
-      </div>
+      {statsByArtist.map((stat) => {
+        const artistName = Object.keys(stat)[0];
+        return (
+          <div
+            className={'d-flex flex-column align-items-center'}
+            style={{ height: '500px' }}
+            key={artistName}
+          >
+            <h3 className={'mt-2'}>Artist: {artistName}</h3>
+            <MainChart mostPopularWords={stat[artistName]} />
+          </div>
+        );
+      })}
     </div>
   );
 };
