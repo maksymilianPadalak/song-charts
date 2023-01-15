a83b88a74db71f9a76971cb651cd9a2c51859532
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-15 15:17:33 +0100

    Add ts-ignore

--- src/components/user/AlbumStats.tsx
+++ src/components/user/AlbumStats.tsx
@@ -22,11 +22,16 @@ const AlbumStats: React.FC<Props> = ({ className }) => {
   if (isLoading) return <Loader fullScreen />;
 
   const stats = data;
+  // @ts-ignore
   const artists: string[] = Object.keys(stats).filter((key) => key !== 'includedSongs');
 
+  // @ts-ignore
   const statsByArtist = artists.map((artist) => ({
+    // @ts-ignore
     [artist]: Object.keys(stats[artist]?.freq)
+      // @ts-ignore
       .map((key) => ({ [key]: stats[artist]?.freq[key] }))
+      // @ts-ignore
       .sort((a, b) => Object.values(b) - Object.values(a))
       .slice(0, 10)
       .reduce((acc, cur) => {
